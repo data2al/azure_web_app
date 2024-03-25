8ad3a357cc230b9a2f966cb7ff78f32ac9a1b576
Data AL <alanluo@outlook.com>
2024-03-25 11:06:08 -0400

    Made adjustments to app and styling

--- app.py
+++ app.py
@@ -7,21 +7,17 @@ app = Flask(__name__)
 @app.route("/")
 #create a home page and return some html
 def mainpage():
-    return render_template("index.html", content = ['Tim', 'Joe', 'Pain'])
-
-@app.route("/home/")
-def home():
-    return "Hello! this is the second page <h1>HELLO<h1>"
+    return render_template("index.html", content = [1,2,3])
 
 # allows text to include user specific info
 @app.route("/user/<name>/")
 def user(name):
-    return f"Hello {name}!"
+    return f"Hello {name}! This app doesn't have an admin page."
 
 # redirect users to a different page
 @app.route("/admin/")
 def admin():
-    return redirect(url_for("user", name = "Admin!"))
+    return redirect(url_for("user", name = "Intruder"))
 
 # have the app run, may need to be removed on azure web app
 if __name__ == "__main__":
